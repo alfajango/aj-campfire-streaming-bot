8ace4992c716dbf6b56a7d2fa6fe4e8c6bc4c273
Steve Schwartz <steve@alfajango.com>
2013-01-04 16:07:38 -0500

    Added twilio code to send sms message

--- stream.rb
+++ stream.rb
@@ -2,14 +2,33 @@ require "rubygems"
 require "bundler/setup"
 require 'twitter/json_stream'
 
+# Campfire config
 token = ENV['CAMPFIRE_TOKEN'] # your API token
 room_id = ENV['CAMPFIRE_ROOM_ID'] # the ID of the room you want to stream
+
+# Twilio config
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_ACCOUNT_TOKEN']
+
+sms_recipient = ENV['SMS_RECIPIENT'] # e.g. '+16105557069'
+sms_sender = ENV['SMS_SENDER'] # e.g. '+14159341234'
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
 
 options = {
   :path => "/room/#{room_id}/live.json",
   :host => 'streaming.campfirenow.com',
     :auth => "#{token}:x"
 }
+
+def send_sms(message)
+  @client.account.sms.messages.create(
+    :from => sms_sender,
+    :to => sms_recipient,
+    :body => message
+  )
+end
 
 EventMachine::run do
   puts "Running and waiting for events to happen..."
